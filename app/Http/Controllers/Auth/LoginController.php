4166d60d991cbf35d382b055fc4de6660a3b4a3b
season-layout <wholelifecanbechangedby1@outlook.jp>
2022-01-07 18:04:26 +0900

    login 遷移先

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      * @var string
      */
     // protected $redirectTo = RouteServiceProvider::HOME;
-    protected $redirectTo = '/register_work';
+    protected $redirectTo = '/toppage';
 
     /**
      * Create a new controller instance.
