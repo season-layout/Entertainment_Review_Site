73ace060cdae1549f55a1b0d684b4137ceba12ad
season-layout <wholelifecanbechangedby1@outlook.jp>
2022-01-13 13:32:09 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if ($this->app->environment() === 'production') {
+        if ($this->app->environment() === 'production' || $this->app->environment() === 'develop') {
             \URL::forceScheme('https');
         }
     }
