376c8d9657436400b309ccd30208ffc166b1f31d
season-layout <wholelifecanbechangedby1@outlook.jp>
2022-01-13 14:59:48 +0900

    composer update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -108,6 +108,6 @@
     </footer>
 
 </div>
-<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 <script src="js/background.js"></script>
 @endsection
